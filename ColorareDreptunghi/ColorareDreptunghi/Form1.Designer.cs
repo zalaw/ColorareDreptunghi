f8c73d7d3e0c55f4930fdae7e25268074db53350
Nuna Razvan <30498668+zalaw@users.noreply.github.com>
2021-01-24 17:17:58 +0200

    Add files via upload

---
+++ ColorareDreptunghi/ColorareDreptunghi/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ColorareDreptunghi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.fillColor = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.X = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Y = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rectangleWidth = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rectangleHeight = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Go = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Culoare";
+            // 
+            // fillColor
+            // 
+            this.fillColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.fillColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fillColor.FormattingEnabled = true;
+            this.fillColor.Location = new System.Drawing.Point(76, 60);
+            this.fillColor.Name = "fillColor";
+            this.fillColor.Size = new System.Drawing.Size(178, 24);
+            this.fillColor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(278, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "X";
+            // 
+            // X
+            // 
+            this.X.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X.Location = new System.Drawing.Point(300, 60);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(67, 22);
+            this.X.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(380, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Y";
+            // 
+            // Y
+            // 
+            this.Y.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Y.Location = new System.Drawing.Point(403, 60);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(67, 22);
+            this.Y.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(487, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Latime";
+            // 
+            // rectangleWidth
+            // 
+            this.rectangleWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rectangleWidth.Location = new System.Drawing.Point(541, 60);
+            this.rectangleWidth.Name = "rectangleWidth";
+            this.rectangleWidth.Size = new System.Drawing.Size(67, 22);
+            this.rectangleWidth.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(620, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Inaltime";
+            // 
+            // rectangleHeight
+            // 
+            this.rectangleHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rectangleHeight.Location = new System.Drawing.Point(680, 60);
+            this.rectangleHeight.Name = "rectangleHeight";
+            this.rectangleHeight.Size = new System.Drawing.Size(67, 22);
+            this.rectangleHeight.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(912, 424);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(354, 31);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Umplere dreptunghi/patrat";
+            // 
+            // Go
+            // 
+            this.Go.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Go.Location = new System.Drawing.Point(762, 60);
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(162, 24);
+            this.Go.TabIndex = 6;
+            this.Go.Text = "Deseneaza";
+            this.Go.UseVisualStyleBackColor = true;
+            this.Go.Click += new System.EventHandler(this.Go_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 548);
+            this.Controls.Add(this.Go);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rectangleHeight);
+            this.Controls.Add(this.rectangleWidth);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.fillColor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox fillColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox X;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Y;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox rectangleWidth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox rectangleHeight;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Go;
+    }
+}
+
---
+++ ColorareDreptunghi/ColorareDreptunghi/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Threading;
+
+namespace ColorareDreptunghi
+{
+    public partial class Form1 : Form
+    {
+        static Bitmap bitmap;
+        static Graphics graphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+            List<Color> colors = new List<Color>();
+
+            foreach (PropertyInfo property in typeof(Color).GetProperties())
+            {
+                if (property.PropertyType == typeof(Color))
+                {
+                    fillColor.Items.Add((Color)property.GetValue(null));
+                }
+            }
+
+            X.Text = "32";
+            Y.Text = "64";
+            rectangleWidth.Text = "100";
+            rectangleHeight.Text = "200";
+        }
+
+        private void Go_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Color color = (Color)fillColor.Items[fillColor.SelectedIndex];
+
+                int x = int.Parse(X.Text);
+                int y = int.Parse(Y.Text);
+
+                int width = int.Parse(rectangleWidth.Text);
+                int height = int.Parse(rectangleHeight.Text);
+
+                graphics.Clear(Color.White);
+
+                DrawMyRectangle(x, y, width, height);
+                FillMyRectangle(x, y, width, height, color);
+
+                pictureBox1.Image = bitmap;
+            }
+            catch
+            {
+                if (fillColor.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Culoarea trebuie selectata!");
+                }
+                else if (!int.TryParse(X.Text, out int x) || !int.TryParse(Y.Text, out int y))
+                {
+                    MessageBox.Show("Coordonatele trebuie sa fie numere intregi!");
+                }
+                else if (!int.TryParse(rectangleWidth.Text, out int width) || !int.TryParse(rectangleHeight.Text, out int height))
+                {
+                    MessageBox.Show("Valorile pentru latime si inaltime trebuie sa fie numere intregi!");
+                }
+            }
+        }
+
+        private void DrawMyRectangle(int x, int y, int width, int height)
+        {
+            for (int i = x; i <= x + width; i++)
+            {
+                bitmap.SetPixel(i, y, Color.Black);
+                bitmap.SetPixel(i, y + height, Color.Black);
+            }
+            for (int i = y; i <= y + height; i++)
+            {
+                bitmap.SetPixel(x + width, i, Color.Black);
+                bitmap.SetPixel(x, i, Color.Black);
+            }
+        }
+
+        private void FillMyRectangle(int x, int y, int width, int height, Color color)
+        {
+            for (int i = y + 1; i < y + height; i++)
+            {
+                for (int j = x + 1; j < x + width; j++)
+                {
+                    bitmap.SetPixel(j, i, color);
+                }
+            }
+        }
+    }
+}
